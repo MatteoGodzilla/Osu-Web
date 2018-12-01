9fe84ba06f7e3df7014febd3ca3eba16806c98ce
MatteoGodzilla <catena.matteo90@gmail.com>
2018-12-01 21:35:56 +0100

    Sketch centrato :)

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 function setup(){
-	createCanvas(windowWidth,windowHeight);
+	c = createCanvas(1024,768);
+	x = (windowWidth - width) / 2;
+	y = (windowHeight - height) / 2;
+	c.position(x,y)
 }
 
 function draw(){
 	background(51);
+	fill(255,255,255,100);
 	ellipse(mouseX,mouseY,50,50);
 }
-
-function windowResized(){
-	resizeCanvas(windowWidth,windowHeight);
-}
-
